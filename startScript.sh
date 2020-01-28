8fca0976ea08a42b3d6387efb177b419e287967f
my-macte-tests <my@mactelabs.com>
2020-01-28 17:48:02 +0700

    commit3

--- startScript.sh
+++ startScript.sh
@@ -1,35 +1,9 @@
 #!/bin/bash
 
-TEXT = "logoutput: /var/log/socks.log
-errorlog: /var/log/socks_error.log
-
-internal: eth0 port = 1080
-external: eth0
-
-
-socksmethod: username
-
-
-
-user.privileged: root
-user.unprivileged: nobody
-user.libwrap: nobody
-
-
-client pass {
-        from: 0/0 to: 0/0
-        log: connect disconnect error ioop
-}
-
-socks pass {
-        from: 0/0 to: 0/0
-        log: connect disconnect error ioop
-}"
-
 sudo apt install dante-server -y
 useradd --shell /usr/sbin/nologin proxy_user_01
 passwd proxy_user_01
-sudo echo -n > /etc/danted.conf -y
-sudo echo $TEXT >> /etc/danted.conf -y
+sudo sh -c 'echo -n > /etc/danted.conf'
+sudo sh -c 'echo "logoutput: /var/log/socks.log/nerrorlog: /var/log/socks_error.log\ninternal: eth0 port = 1080\nexternal: eth0\nsocksmethod: username\nuser.privileged: root/nuser.unprivileged: nobody/nuser.libwrap: nobody\nclient pass {\nfrom: 0/0 to: 0/0\nlog: connect disconnect error ioop\n}\nsocks pass {\nfrom: 0/0 to: 0/0\nlog: connect disconnect error ioop\n}" >> /etc/danted.conf'
 systemctl enable danted
 systemctl start danted
